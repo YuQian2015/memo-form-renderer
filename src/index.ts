7cb31f16327c8a08af0d8cab6e91d97507494a3f
“Yu” <yuqian.124@gmail.com>
2024-11-28 17:55:00 +0800

    feat: update package version to 0.0.9 and migrate index file from .ts to .tsx

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createRequiredLayout, createExposedLayout } from "./components/form/index";
-import FormRenderer, { FormRendererHandle } from "./components/form/FormRenderer";
-
-export { createRequiredLayout, createExposedLayout, FormRenderer };
-
-export type { FormRendererHandle };
-
-export type { AimForm } from "./components/form/types";
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+export { createRequiredLayout, createExposedLayout } from "./components/form/index";
+export type { FormRendererHandle } from "./components/form/FormRenderer";
+
+export { default as FormRenderer } from "./components/form/FormRenderer";
+
+export type { AimForm } from "./components/form/types";
